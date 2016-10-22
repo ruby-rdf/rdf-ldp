952eaf5479fc515fa52ffa27840ad3b7f4d0c3b1
Tom Johnson <tom@dp.la>
2016-10-22 15:58:48 -0700

    [Memento] Add `#memento_versions` for listing versions

--- lib/rack/memento/timemap.rb
+++ lib/rack/memento/timemap.rb
@@ -3,13 +3,21 @@ module Rack
     ##
     # Mixin behavior for Memento TimeMaps
     #
-    # Implementing classes are expected to provide a `@memento_original` instance variable and a `#to_uri`.
+    # Implementing classes are expected to provide a `@memento_original`
+    # instance variable, a `#to_uri` method, and a `#versions` method returning
+    # an enumerable of version uris.
     module Timemap
       # @!attribute [r] memento_original
       #   @return [RDF::URI] the uri identifying the original resource (URI-R)
       # @!attribute [r] to_uri
       #   @return [RDF::URI] the uri identifying this (TimeMap) resource
       attr_reader :memento_original
+
+      ##
+      # @return [#each] a Rack response suitable for `link-format`
+      # @see http://www.ietf.org/rfc/rfc6690.txt for info about CoRE Link Format
+      # def link_format
+      # end
 
       ##
       # @return [String] the uri identifying this resource in string form
