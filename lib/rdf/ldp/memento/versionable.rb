c7599b56d577626bf6e5cbe8030c13e977f9e447
Tom Johnson <tom@dp.la>
2016-11-05 13:55:54 -0700

    [Memento] Support TimeMap from Versionable

--- lib/rdf/ldp/memento/versionable.rb
+++ lib/rdf/ldp/memento/versionable.rb
@@ -1,11 +1,16 @@
+require 'rdf/ldp/memento/version_container'
+
 module RDF::LDP::Memento
   ##
   # Mixin to `RDF::LDP::Resource` adding Memento versioning support. The 
-  # interfaces implemented here are compatible with `Rack::Memento` and the fcrepo4 API Specification.
+  # interfaces implemented here are compatible with `Rack::Memento` and 
+  # the fcrepo4 API Specification.
   # 
   # @see RDF::LDP::Resource
   # @see Rack::Memento
   module Versionable
+    TIMEMAP_CONTAINER_CLASS = RDF::LDP::Memento::VersionContainer
+
     ##
     # @return []
     def timegate
@@ -14,9 +19,19 @@ module RDF::LDP::Memento
 
     ##
     # @return [VersionContainer]
-    # def timemap
+    def timemap
+      @timemap ||= RDF::LDP::Resource.find(timemap_uri, @data)
+    rescue RDF::LDP::NotFound
+      @timemap = TIMEMAP_CONTAINER_CLASS.new(timemap_uri, @data)
+    end
+    alias_method :version_container, :timemap
+
+    ##
+    # Build a timemap uri
     #
-    # end
-    # alias_method :version_container, :timemap
+    # @param uri [RDF::URI]
+    def timemap_uri
+      subject_uri / '.well-known/timemap'
+    end
   end
 end
--- spec/lib/rdf/ldp/memento/versionable_spec.rb
+++ spec/lib/rdf/ldp/memento/versionable_spec.rb
@@ -1,23 +1,28 @@
 # coding: utf-8
+require 'spec_helper'
+
 require 'rdf/ldp'
 require 'rdf/ldp/memento/versionable'
 
 describe RDF::LDP::Memento::Versionable do
-  subject   { resource_class.new(uri) }
-  let(:uri) { 'http://example.org/moomin' }
-
   let(:resource_class) do
     Class.new(RDF::LDP::Resource) do
       include RDF::LDP::Memento::Versionable
     end
   end
 
-  describe '#timegate' do
-    it 'defaults to self' do
-      expect(subject.timegate).to equal subject
-    end
-  end
+  subject   { resource_class.new(uri) }
+  let(:uri) { 'http://example.org/moomin' }
+
+  it_behaves_like 'a versionable LDP-R'
 
   describe '#timemap' do
+    it 'has a .well-known uri' do
+      expect(subject.timemap.to_uri.to_s).to include '.well-known/timemap'
+    end
+    
+    it 'returns the same timemap repeatedly' do
+      expect(subject.timemap).to equal subject.timemap
+    end
   end
 end
